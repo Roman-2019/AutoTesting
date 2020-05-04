6907d7286addbe1040cff3e171015b19fcb1c785
Roman-2019 <58485070+Roman-2019@users.noreply.github.com>
2020-05-04 16:19:38 +0300

    AddUnitsRoots

--- DiscriminantDll_Tests/MSUnitTestsRoots.cs
+++ DiscriminantDll_Tests/MSUnitTestsRoots.cs
@@ -36,5 +36,54 @@ namespace DiscriminantDll_Tests
             Assert.IsTrue(d.x1<0, d.x1.ToString());
             Assert.IsTrue(d.x2<0, d.x2.ToString());
         }
+
+        [TestMethod]
+        public void Test_Discriminant_10_10_1()
+        {
+            double a = 10;
+            double b = 10;
+            double c = 1;
+
+            var d = _discriminant.CalculationDiscriminant(a, b, c);
+
+            Assert.IsTrue(d.x1 < 0 || d.x2 < 0);           
+        }
+
+        [TestMethod]
+        public void Test_Discriminant_1_1_1()
+        {
+            double a = 1;
+            double b = 1;
+            double c = 1;
+
+            var d = _discriminant.CalculationDiscriminant(a, b, c);
+
+            Assert.IsTrue(d.x1 == 0 && d.x2 == 0);          
+        }
+
+        [TestMethod]
+        public void Test_Discriminant_0_1_1()
+        {
+            double a = 0;
+            double b = 1;
+            double c = 1;
+
+            var d = _discriminant.CalculationDiscriminant(a, b, c);
+
+            Assert.IsTrue(d.x1 == 0 && d.x2 == 0);
+        }
+
+        [TestMethod]
+        public void Test_Discriminant_1_2_1()
+        {
+            double a = 1;
+            double b = 2;
+            double c = 1;
+
+            var d = _discriminant.CalculationDiscriminant(a, b, c);
+
+            Assert.IsTrue(d.x1 != 0 && d.x2 == 0);
+        }
+
     }
 }
--- Discriminant_dll/Discriminant.cs
+++ Discriminant_dll/Discriminant.cs
@@ -12,9 +12,38 @@ namespace Discriminant_dll
         {
             var _discriminant = (D:0.0, x1:0.0, x2:0.0);
             _discriminant.D = Math.Pow(b, 2) - 4 * a * c;
-            _discriminant.x1 = -Math.Round((b + Math.Sqrt(_discriminant.D)) / (2 * a),2);
-            _discriminant.x2 = -Math.Round((b - Math.Sqrt(_discriminant.D)) / (2 * a),2);
 
+            if (_discriminant.D < 0)
+            {
+                _discriminant.x1 = 0;
+                _discriminant.x2 = 0;
+            }
+            else if (_discriminant.D > 0)
+            {
+                if (a == 0)
+                {
+                    _discriminant.x1 = 0;
+                    _discriminant.x2 = 0;
+                }
+                else
+                {
+                    _discriminant.x1 = -Math.Round((b + Math.Sqrt(_discriminant.D)) / (2 * a), 2);
+                    _discriminant.x2 = -Math.Round((b - Math.Sqrt(_discriminant.D)) / (2 * a), 2);
+                }
+            }
+            else 
+            {
+                if (a == 0)
+                {
+                    _discriminant.x1 = 0;
+                    _discriminant.x2 = 0;
+                }
+                else
+                {
+                    _discriminant.x1 = -Math.Round((b + Math.Sqrt(_discriminant.D)) / (2 * a), 2);
+                    _discriminant.x2 = 0;
+                }
+            }
             //var temp = _discriminant.x1.ToString();
 
             return _discriminant;
